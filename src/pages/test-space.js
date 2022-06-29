c1bf27f55b05649ad7742c084c7a99faee95000f
brightGarden02 <rreone2017@gmail.com>
2022-06-30 00:01:15 +0900

    테스트 페이지 추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TestSpacePage() {
+  return (
+    <main>
+      <title>테스트</title>
+      <h1>테스트 스페이스 페이지 입니다/TEST SPACE PAGE</h1>
+    </main>
+  );
+}
+
+export default TestSpacePage;
---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TestPage() {
+  return (
+    <main>
+      <title>테스트</title>
+      <h1>테스트 페이지 입니다/TEST</h1>
+    </main>
+  );
+}
+
+export default TestPage;
