4df48df161aa6a5c4ecb5fec63bae39c28504f71
brightGarden02 <rreone2017@gmail.com>
2022-06-29 23:29:07 +0900

    about 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page?!?!?!</title>
+      <title>Home Page :)</title>
       <h1>Home Page!!! WOW</h1>
     </main>
   );
