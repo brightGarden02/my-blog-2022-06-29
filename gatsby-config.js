30c152dc383bf49600f896ec319d4fe0561c6a0d
brightGarden02 <rreone2017@gmail.com>
2022-07-01 22:09:59 +0900

    blog 디렉토리에 mdx 파일들 생성, graphql로 읽기

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `My Second Gatsby Site`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
