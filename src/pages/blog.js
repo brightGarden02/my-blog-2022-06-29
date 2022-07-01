81c54b52102ceed9d22675ee2e2f9a508cda998e
brightGarden02 <rreone2017@gmail.com>
2022-07-01 22:19:41 +0900

    Make lists in  My Blog Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,27 @@
 import Layout from "../components/layout";
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
 function BlogPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile {
+        nodes {
+          name
+        }
+      }
+    }
+  `);
+
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My Cool posts will go in here</p>
+
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
