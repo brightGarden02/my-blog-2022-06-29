ff6d05e1803bafb391523c104775c4937603249f
brightGarden02 <rreone2017@gmail.com>
2022-07-01 20:50:50 +0900

    StaticImg 컴포넌트로 이미지 노출

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 
@@ -5,6 +6,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Hi there </p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 };
