610343bc8afc0b2dbf6ace9f9aa22c7ba0188c3c
brightGarden02 <rreone2017@gmail.com>
2022-07-02 14:26:02 +0900

    Make links to post page in MY Blog Posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,35 +1,35 @@
+import * as React from "react";
+import { Link, graphql } from "gatsby";
 import Layout from "../../components/layout";
-import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
 
-function BlogPage() {
-  const data = useStaticQuery(graphql`
-    query {
-      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-        nodes {
-          frontmatter {
-            date(formatString: "MMMM D, YYYY")
-            title
-          }
-          id
-          body
-        }
-      }
-    }
-  `);
-
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>{node.body}</MDXRenderer>
         </article>
       ))}
     </Layout>
   );
-}
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
 
 export default BlogPage;
