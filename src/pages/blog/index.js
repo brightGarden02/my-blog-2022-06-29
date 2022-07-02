ae061853125d374aacd73e8e63d84a48a0fa04e9
brightGarden02 <rreone2017@gmail.com>
2022-07-02 13:58:58 +0900

    Add Blog Post Page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
